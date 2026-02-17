9d9922ed4ec54985da038d191a0382cb9c647dcb
ilikememes5209 <coltonblake125@outlook.com>
2026-02-17 10:11:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+ function hideHeader() {
+    let headerElement = document.getElementById("dismissible-header");
+    // Add the 'hidden' class to the header to hide it
+    headerElement.classList.add("hidden");
+}
